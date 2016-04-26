e5a3f7b174e1809be7d99f04726982e638710458
masakuri <m.kurihara777@gmail.com>
2016-04-26 16:02:17 +0900

    changed all

--- bin/mk40-49/mk43.py
+++ bin/mk40-49/mk43.py
@@ -5,41 +5,29 @@
 名詞を含む文節が，動詞を含む文節に係るとき，これらをタブ区切り形式で抽出せよ．ただし，句読点などの記号は出力しないようにせよ．
 """
 
-import sys
-import mk42
-import MeCab
-
-data = mk42.dst_srcs(sys.stdin)
-m = MeCab.Tagger()
-for sentense in data:
-    for index in sentense:
-        if index[2] == "-1":
-            break
-        else:
-            if index[1] == "":
-                continue
-            else:
-                if m.parse(index[1]).split()[1].split(",")[0] == "名詞" and m.parse(sentense[int(index[2])][1]).split()[1].split(",")[0] == "動詞":
-                    print index[1]+ "\t" + sentense[int(index[2])][1]
-
 """
+chunk = [morphs=[[surface, base, pos, pos1], ...], dst, srcs, phrase]
+chunk = [[[我輩, 我輩, 名詞, 代名詞], [は, は, 助詞, 助詞]], 2, [0], 我輩は]
+"""
+
 import sys, re
 import mk41
 
 data = mk41.load_cabocha(sys.stdin)
-i = 0
-for sentense in data:
-    for chunk in sentense:
+pattern = re.compile(r"。|、|　")
+
+for sentence in data:
+    for chunk in sentence:
         for morpheme in chunk.morphs:
-            print morpheme.surface
-            # if morpheme.pos == "名詞" and i == 0:
-            #     print morpheme.surface
-            #     i += 1
-            # elif morpheme.pos == "動詞" and i == 1:
-            #     print morpheme.surface
-            #     i = 0
-        print "*****"
-"""
+            if morpheme.pos == "名詞" and chunk.dst != -1:
+                noun_src = pattern.sub("",chunk.phrase) # 句読点等削除
+                for is_verb_morphs in sentence[chunk.dst].morphs:
+                    if is_verb_morphs.pos == "動詞":
+                        verb_dst = pattern.sub("", sentence[chunk.dst].phrase)  # 句読点等削除
+                        print noun_src + "\t" + verb_dst
+                        break
+                break
+
 
 """
 $ python mk43.py < neko.txt.cabocha
@@ -47,11 +35,15 @@ $ python mk43.py < neko.txt.cabocha
 見当が  つかぬ
 所で    泣いて
 ニャーニャー    泣いて
+いた事だけは    記憶している
 吾輩は  見た
 ここで  始めて
 ものを  見た
 あとで  聞くと
 我々を  捕えて
 掌に    載せられて
+スーと  持ち上げられた
+時      フワフワした
+感じが  あったばかりである
 ...
 """
