296c8497a929a7fee7f5ac5ccb2262bac04bb435
masakuri <m.kurihara777@gmail.com>
2016-05-10 17:55:29 +0900

    added mk50.py

--- bin/mk50-59/mk50.py
+++ bin/mk50-59/mk50.py
@@ -1,7 +1,28 @@
 # coding=utf-8
 
-import re
+"""
+50. 文区切り
+(. or ; or : or ? or !) → 空白文字 → 英大文字というパターンを文の区切りと見なし，入力された文書を1行1文の形式で出力せよ．
+"""
 
-with open("../../data/nlp.txt", "r") as f:
-    pattern = re.compile(r"(\.|\;|\?|\!)\ [A-Z]")
-    
+import re, sys
+
+f = sys.stdin
+pattern = re.compile(r"([A-Z].+?[.;:?!])[ \n]")
+for paragraph in f:
+    for m in pattern.finditer(paragraph):
+        print m.group()
+
+"""
+$ python mk50.py < nlp.txt > result50.txt
+$ less result50.txt
+Natural language processing (NLP) is a field of computer science, artificial intelligence, and linguistics concerned with the interactions between computers and human (natural) languages.
+As such, NLP is related to the area of humani-computer interaction.
+Many challenges in NLP involve natural language understanding, that is, enabling computers to derive meaning from human or natural language input, and others involve natural language generation.
+
+The history of NLP generally starts in the 1950s, although work can be found from earlier periods.
+In 1950, Alan Turing published an article titled "Computing Machinery and Intelligence" which proposed what is now called the Turing test as a criterion of intelligence.
+
+The Georgetown experiment in 1954 involved fully automatic translation of more than sixty Russian sentences into English.
+...
+"""
