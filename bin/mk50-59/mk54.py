37429d87f13c392a663edebdba1c9dcd4302e089
masakuri <m.kurihara777@gmail.com>
2016-05-11 16:10:13 +0900

    added mk54.py

---
+++ bin/mk50-59/mk54.py
@@ -0,0 +1,59 @@
+# coding=utf-8
+
+"""
+54. 品詞タグ付け
+Stanford Core NLPの解析結果XMLを読み込み，単語，レンマ，品詞をタブ区切り形式で出力せよ．
+"""
+
+from xml.etree.ElementTree import *
+import sys
+
+tree = parse(sys.stdin)
+elem = tree.getroot()
+for w, l, p in zip(elem.findall(".//word"), elem.findall(".//lemma"), elem.findall(".//POS")):
+    print w.text + "\t" + l.text + "\t" + p.text
+
+"""
+$ python mk54.py < nlp.txt.out
+Natural natural JJ
+language        language        NN
+processing      processing      NN
+From    from    IN
+Wikipedia       Wikipedia       NNP
+,       ,       ,
+the     the     DT
+free    free    JJ
+encyclopedia    encyclopedia    NN
+Natural natural JJ
+language        language        NN
+processing      processing      NN
+-LRB-   -lrb-   -LRB-
+NLP     nlp     NN
+-RRB-   -rrb-   -RRB-
+is      be      VBZ
+a       a       DT
+field   field   NN
+of      of      IN
+computer        computer        NN
+science science NN
+,       ,       ,
+artificial      artificial      JJ
+intelligence    intelligence    NN
+,       ,       ,
+and     and     CC
+linguistics     linguistics     NNS
+concerned       concern VBN
+with    with    IN
+the     the     DT
+interactions    interaction     NNS
+between between IN
+computers       computer        NNS
+and     and     CC
+human   human   JJ
+-LRB-   -lrb-   -LRB-
+natural natural JJ
+-RRB-   -rrb-   -RRB-
+languages       language        NNS
+.       .       .
+...
+"""
