541f71b5f0543bf17f29ade871e4583753698901
masakuri <m.kurihara777@gmail.com>
2016-05-11 16:09:19 +0900

    added mk53.py

---
+++ bin/mk50-59/mk53.py
@@ -0,0 +1,65 @@
+# coding=utf-8
+
+"""
+53. Tokenization
+Stanford Core NLPを用い，入力テキストの解析結果をXML形式で得よ．また，このXMLファイルを読み込み，入力テキストを1行1単語の形式で出力せよ．
+"""
+
+"""
+(stanford-corenlp-full-2015-12-09/ディレクトリ内で)
+$ java -cp "*" -Xmx2g edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,lemma,ner,parse,dcoref -file nlp.txt
+-> nlp.txt.outのXMLファイル
+"""
+
+from xml.etree.ElementTree import *
+import sys
+
+tree = parse(sys.stdin)
+elem = tree.getroot()
+for e in elem.findall(".//word"):
+    print e.text
+
+"""
+$ python mk53.py < nlp.txt.out
+Natural
+language
+processing
+From
+Wikipedia
+,
+the
+free
+encyclopedia
+Natural
+language
+processing
+-LRB-
+NLP
+-RRB-
+is
+a
+field
+of
+computer
+science
+,
+artificial
+intelligence
+,
+and
+linguistics
+concerned
+with
+the
+interactions
+between
+computers
+and
+human
+-LRB-
+natural
+-RRB-
+languages
+.
+...
+"""
