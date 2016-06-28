012276f556ce3d7293eaa9adcc1610d22fdf2e3e
masakuri <m.kurihara777@gmail.com>
2016-06-28 14:21:16 +0900

    change from tuple to list

--- bin/mk50-59/mk57.py
+++ bin/mk50-59/mk57.py
@@ -5,6 +5,19 @@
 Stanford Core NLPの係り受け解析の結果（collapsed-dependencies）を有向グラフとして可視化せよ．可視化には，係り受け木をDOT言語に変換し，Graphvizを用いるとよい．また，Pythonから有向グラフを直接的に可視化するには，pydotを使うとよい．
 """
 
+"""
+<dependencies type="collapsed-dependencies">
+  <dep type="root">
+    <governor idx="0">ROOT</governor>
+    <dependent idx="18">field</dependent>
+  </dep>
+  <dep type="amod">
+    <governor idx="3">processing</governor>
+    <dependent idx="1">Natural</dependent>
+  </dep>
+  ...
+"""
+
 import xml.etree.ElementTree as ET
 import sys
 import pydot
@@ -13,19 +26,20 @@ tree = ET.parse(sys.stdin)
 elem = tree.getroot()
 collapsed_dependencies = elem.findall(".//dependencies[@type='collapsed-dependencies']")
 det_src_ls = list()
+node_ls = list()
 sentence_ls = list()
-ls = list()
+
 for dependency in collapsed_dependencies:
     for dep in dependency:
         if dep.find("dependent").text != "." and dep.find("dependent").text != ",":
             det_src_ls.append(dep.find("governor").text)
             det_src_ls.append(dep.find("dependent").text)
-            sentence_ls.append(tuple(det_src_ls))
+            node_ls.append(det_src_ls)
             det_src_ls = list()
-    ls.append(sentence_ls)
-    sentence_ls = list()
+    sentence_ls.append(node_ls)
+    node_ls = list()
 
-g=pydot.graph_from_edges(ls[1], directed=True)
+g=pydot.graph_from_edges(sentence_ls[1], directed=True)
 g.write_jpeg('result57.jpg', prog='dot')
 
 """
